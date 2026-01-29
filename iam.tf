3b911039220cf314d3c084ae01e96b70bc0c27be
IbrahimSufian1 <ibraheemsufian57@gmail.com>
2026-01-29 23:18:39 +0300

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -18,4 +18,26 @@ resource "aws_iam_role_policy_attachment" "ssm_policy" {
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "schedemy-ec2-instance-profile"
   role = aws_iam_role.ec2_role.name
+
+}
+resource "aws_iam_role_policy" "s3_access_policy" {
+  name = "schedemy-s3-access"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.app_bucket.arn,
+          "${aws_s3_bucket.app_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
 }
