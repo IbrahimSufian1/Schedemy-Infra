49283695ad52c465296847bfd63980f76bbf9f8c
IbrahimSufian1 <ibraheemsufian57@gmail.com>
2026-01-29 23:19:10 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "ami_id" {
   description = "The Golden AMI ID for SSchedemy Backend"
   type = string
-  default = "ami-0bd34c6ba80e09e36"
+  default = "ami-0b39875d661541e13"
 }
 variable "instance_type" {
   default = "t3.micro"
@@ -10,3 +10,4 @@ variable "key_name" {
   default = "SchedServer"
 
 }
+
